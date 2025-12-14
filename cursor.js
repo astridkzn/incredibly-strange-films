f6d42913a06996936a5c968cad8909c375daca79
astridkzn <astridkzn@gmail.com>
2025-12-15 00:21:52 +0100

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,3 +1,4 @@
+<script>
 const OPEN_CURSOR =
   "url('https://raw.githubusercontent.com/astridkzn/incredibly-strange-films/main/images/open.png') 16 16, auto";
 
@@ -7,7 +8,13 @@ const CLOSE_CURSOR =
 // curseur par défaut
 document.body.style.cursor = OPEN_CURSOR;
 
-// au clic → close
-document.addEventListener("click", () => {
+// ajoute une classe au clic
+document.addEventListener("mousedown", () => {
   document.body.style.cursor = CLOSE_CURSOR;
 });
+
+// remet le curseur à l'état open quand on relâche
+document.addEventListener("mouseup", () => {
+  document.body.style.cursor = OPEN_CURSOR;
+});
+</script>
